82bccd898eb5bd2085f17376ae6112000bba15c1
tangjijun <tangjijun@yhd.com>
2017-09-05 13:56:58 +0800

    ranger partitioner

--- src/main/scala/com/madhukaraphatak/examples/sparktwo/Rangepartitioning.scala
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/Rangepartitioning.scala
@@ -1,22 +1,27 @@
 package com.madhukaraphatak.examples.sparktwo
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.Row
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Row, SparkSession}
 //https://issues.apache.org/jira/browse/SPARK-17788
 //https://issues.apache.org/jira/browse/SPARK-9862  Join: Handling data skew
 object Rangepartitioning {
   def main(args: Array[String]): Unit = {
 
-   /* val conf = new SparkConf().setAppName("sortsample")
+    val conf = new SparkConf().setAppName("sortsample")
     conf.setMaster("local[4]")
     conf.set("spark.ui.port", "2029")
     // Set the partitions and parallelism to relatively low value so we can read the results.
     conf.set("spark.default.parallelism", "20")
     conf.set("spark.sql.shuffle.partitions", "20")
-    val spark = new SparkContext(conf)
+    val spark = SparkSession.builder
+      .config(conf)
+      .getOrCreate()
+
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
     // Create a skewed data frame.
     val df = spark
-      .range(0,10000000)
+      .range(10000000)
       .select(
         $"id",
         (rand(34) * when($"id" % 10 <= 7, lit(1.0)).otherwise(lit(10.0))).as("value"))
@@ -30,7 +35,12 @@ object Rangepartitioning {
           (count + 1L, Math.min(min, value), Math.max(max, value))
       }
       Iterator.single(PartitionSummary(count, min, max, max - min))
-    }*/
+    }
+
+    // Get results and make them look nice
+    res.orderBy($"min")
+      .select($"count", $"min".cast("decimal(5,3)"), $"max".cast("decimal(5,3)"), $"range".cast("decimal(5,3)"))
+      .show(30)
   }
 
 }
