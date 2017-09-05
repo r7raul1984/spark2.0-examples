71f57c0b9432e1e3361c61369026640661cf3786
tangjijun <tangjijun@yhd.com>
2017-09-05 10:30:13 +0800

    ranger partitioner

---
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/Rangepartitioning.scala
@@ -0,0 +1,35 @@
+package com.madhukaraphatak.examples.sparktwo
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.Row
+//https://issues.apache.org/jira/browse/SPARK-17788
+object Rangepartitioning {
+  def main(args: Array[String]): Unit = {
+
+   /* val conf = new SparkConf().setAppName("sortsample")
+    conf.setMaster("local[4]")
+    conf.set("spark.ui.port", "2029")
+    // Set the partitions and parallelism to relatively low value so we can read the results.
+    conf.set("spark.default.parallelism", "20")
+    conf.set("spark.sql.shuffle.partitions", "20")
+    val spark = new SparkContext(conf)
+    // Create a skewed data frame.
+    val df = spark
+      .range(0,10000000)
+      .select(
+        $"id",
+        (rand(34) * when($"id" % 10 <= 7, lit(1.0)).otherwise(lit(10.0))).as("value"))
+
+    // Make a summary per partition. The partition intervals should not overlap and the number of
+    // elements in a partition should roughly be the same for all partitions.
+    case class PartitionSummary(count: Long, min: Double, max: Double, range: Double)
+    val res = df.orderBy($"value").mapPartitions { iterator =>
+      val (count, min, max) = iterator.foldLeft((0L, Double.PositiveInfinity, Double.NegativeInfinity)) {
+        case ((count, min, max), Row(_, value: Double)) =>
+          (count + 1L, Math.min(min, value), Math.max(max, value))
+      }
+      Iterator.single(PartitionSummary(count, min, max, max - min))
+    }*/
+  }
+
+}
--- src/main/scala/com/madhukaraphatak/examples/sparktwo/SortSample.scala
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/SortSample.scala
@@ -1,17 +1,19 @@
 package com.madhukaraphatak.examples.sparktwo
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object SortSample {
   def main(args: Array[String]) {
 
     val conf = new SparkConf().setAppName("sortsample")
-    conf.setAppName("test")
     conf.setMaster("local[4]")
-    conf.set("spark.ui.port","2029")
+    conf.set("spark.ui.port", "2029")
     val sc = new SparkContext(conf)
-    var pairs = sc.parallelize(Array(("e",0),("b",0),("c",3),("d",6),("e",0),("f",0),("g",3),("h",6)), 2);
-    pairs.sortByKey(true, 3).collect().foreach(println);
+    var pairs = sc.parallelize(Array(("e", 0), ("b", 0), ("c", 3), ("d", 6), ("e", 0), ("f", 0), ("g", 3), ("h", 6)), 2)
+    var rs = pairs.sortByKey(true, 3)
+    rs.collect().foreach(println)
+    println(rs.toDebugString)
+    Thread.sleep(30000000)
+    sc.stop()
   }
 }
